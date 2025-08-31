b58016e5f10ed38ce8bc39701307366f3a3eeb2d
mansii1 <mansimangukiya0@gmail.com>
2025-08-31 21:31:58 +0530

    Completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 import "./style.css";
+
+const counters = document.querySelectorAll<HTMLElement>(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+
+  const updateCounter = () => {
+    const targetAttr = counter.getAttribute("data-target");
+    const target = targetAttr ? +targetAttr : 0;
+    const c = +counter.innerText;
+
+    const increment = target / 200;
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = `${target}`;
+    }
+  };
+
+  updateCounter();
+});
